90337cc131bacdb4a5b77fa210c8ae9a07870af5
vicsnet <adesanmiadebisi@gmail.com>
2022-09-20 23:10:54 +0100

    item remove

--- index.js
+++ index.js
@@ -32,137 +32,203 @@ const handleSubmit = (e) => {
     localStorage.setItem("users", JSON.stringify(user_records));
   }
 
-
-    
-    e.preventDefault();
+  e.preventDefault();
 };
 
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 1)
-  ) {
-    document.getElementById("text1").innerHTML = "Booked";
-    document.getElementById("my").style.backgroundColor = "lightblue";
-}
-  
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 2)
-  ) {
-    document.getElementById("text2").innerHTML = "Booked";
-    document.getElementById("my1").style.backgroundColor = "lightblue";
+const checkout = (e) => {
+  // localstorage_value.textContent = localStorage.getItem("users");
+  // console.log(localstorage_value.textContent);
+
+  // console.log('')
+
+  let date = new Date();
+  let time = date.getHours() + " :" + date.getMinutes();
+  let name, size, space;
+
+  name = document.myForm.name.value;
+  size = document.myForm.size.value;
+  space = document.myForm.space.value;
+
+  let user_records = JSON.parse(localStorage.getItem("users")) || [];
+  let exist =
+    user_records.length &&
+    JSON.parse(localStorage.getItem("users")).some(
+      (data) => data.name == name && data.size == size && data.space == space
+      // data.time == time
+    );
+
+  if (!exist) {
+    alert("Incorrect parking details");
+  } else {
+    localStorage.removeItem('users');
+    alert("thanks for paking with us");
   }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 3)
-  ) {
-    document.getElementById("text3").innerHTML = "Booked";
-    document.getElementById("my2").style.backgroundColor = "lightblue";
+  e.preventDefault();
+};
+
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 1
+  )
+) {
+  document.getElementById("text1").innerHTML = "Booked";
+  document.getElementById("my").style.backgroundColor = "lightblue";
 }
-  
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 4)
-  ) {
-    document.getElementById("text4").innerHTML = "Booked";
-    document.getElementById("my3").style.backgroundColor = "lightblue";
-  }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 5)
-  ) {
+
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 2
+  )
+) {
+  document.getElementById("text2").innerHTML = "Booked";
+  document.getElementById("my1").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 3
+  )
+) {
+  document.getElementById("text3").innerHTML = "Booked";
+  document.getElementById("my2").style.backgroundColor = "lightblue";
+}
+
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 4
+  )
+) {
+  document.getElementById("text4").innerHTML = "Booked";
+  document.getElementById("my3").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 5
+  )
+) {
   document.getElementById("text5").innerHTML = "Booked";
   document.getElementById("my4").style.backgroundColor = "lightblue";
-  }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 6)
-  ) {
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 6
+  )
+) {
   document.getElementById("text6").innerHTML = "Booked";
   document.getElementById("my5").style.backgroundColor = "lightblue";
-  }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 7)
-  ) {
-document.getElementById("text7").innerHTML = "Booked";
-document.getElementById("my6").style.backgroundColor = "lightblue";
-  }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 8)
-  ) {
- document.getElementById("text8").innerHTML = "Booked";
- document.getElementById("my7").style.backgroundColor = "lightblue";
-  }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 9)
-  ) {
- document.getElementById("text9").innerHTML = "Booked";
- document.getElementById("my8").style.backgroundColor = "lightblue";
-  }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 10)
-  ) {
- document.getElementById("text10").innerHTML = "Booked";
- document.getElementById("my9").style.backgroundColor = "lightblue";
 }
-  
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 11)
-  ) {
- document.getElementById("text11").innerHTML = "Booked";
- document.getElementById("my10").style.backgroundColor = "lightblue";
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 7
+  )
+) {
+  document.getElementById("text7").innerHTML = "Booked";
+  document.getElementById("my6").style.backgroundColor = "lightblue";
 }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 1 && data.space == 12)
-  ) {
- document.getElementById("text12").innerHTML = "Booked";
- document.getElementById("my11").style.backgroundColor = "lightblue";
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 8
+  )
+) {
+  document.getElementById("text8").innerHTML = "Booked";
+  document.getElementById("my7").style.backgroundColor = "lightblue";
 }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 13)
-  ) {
-document.getElementById("text13").innerHTML = "Booked";
-document.getElementById("my12").style.backgroundColor = "lightblue";
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 9
+  )
+) {
+  document.getElementById("text9").innerHTML = "Booked";
+  document.getElementById("my8").style.backgroundColor = "lightblue";
 }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 14)
-  ) {
-document.getElementById("text14").innerHTML = "Booked";
-document.getElementById("my13").style.backgroundColor = "lightblue";
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 10
+  )
+) {
+  document.getElementById("text10").innerHTML = "Booked";
+  document.getElementById("my9").style.backgroundColor = "lightblue";
 }
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 15)
-  ) {
-document.getElementById("text15").innerHTML = "Booked";
-document.getElementById("my14").style.backgroundColor = "lightblue";
-}
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 16)
-  ) {
-document.getElementById("text16").innerHTML = "Booked";
-document.getElementById("my15").style.backgroundColor = "lightblue";
-}
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 17)
-  ) {
-document.getElementById("text17").innerHTML = "Booked";
-document.getElementById("my16").style.backgroundColor = "lightblue";
-}
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 18)
-  ) {
- document.getElementById("text18").innerHTML = "Booked";
- document.getElementById("my17").style.backgroundColor = "lightblue";
-}
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 19)
-  ) {
-document.getElementById("text19").innerHTML = "Booked";
-document.getElementById("my18").style.backgroundColor = "lightblue";
-}
-  if (
-    JSON.parse(localStorage.getItem("users")).some((data) => data.size == 2 && data.space == 10)
-  ) {
-document.getElementById("text20").innerHTML = "Booked";
-document.getElementById("my19").style.backgroundColor = "lightblue";
-}
-  
 
-
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 11
+  )
+) {
+  document.getElementById("text11").innerHTML = "Booked";
+  document.getElementById("my10").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 1 && data.space == 12
+  )
+) {
+  document.getElementById("text12").innerHTML = "Booked";
+  document.getElementById("my11").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 13
+  )
+) {
+  document.getElementById("text13").innerHTML = "Booked";
+  document.getElementById("my12").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 14
+  )
+) {
+  document.getElementById("text14").innerHTML = "Booked";
+  document.getElementById("my13").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 15
+  )
+) {
+  document.getElementById("text15").innerHTML = "Booked";
+  document.getElementById("my14").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 16
+  )
+) {
+  document.getElementById("text16").innerHTML = "Booked";
+  document.getElementById("my15").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 17
+  )
+) {
+  document.getElementById("text17").innerHTML = "Booked";
+  document.getElementById("my16").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 18
+  )
+) {
+  document.getElementById("text18").innerHTML = "Booked";
+  document.getElementById("my17").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 19
+  )
+) {
+  document.getElementById("text19").innerHTML = "Booked";
+  document.getElementById("my18").style.backgroundColor = "lightblue";
+}
+if (
+  JSON.parse(localStorage.getItem("users")).some(
+    (data) => data.size == 2 && data.space == 10
+  )
+) {
+  document.getElementById("text20").innerHTML = "Booked";
+  document.getElementById("my19").style.backgroundColor = "lightblue";
+}
 
 // const book1 = () => {
 //   document.getElementById("text1").innerHTML = "Booked";
